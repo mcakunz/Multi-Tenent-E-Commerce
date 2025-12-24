24ce6a6019312f0e58e6247050bbb9262f2cb99d
mcakunz <matheus.kunz05@gmail.com>
2025-12-24 17:07:57 -0300

    feat: categories seed and responsive navigation - add seed script for categories - add db:seed command - create CustomCategory type - hide overflow categories - add mobile categories sidebar

--- multi-tenent-e-commerce/src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ multi-tenent-e-commerce/src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -6,9 +6,10 @@ import { Category } from "@/payload-types";
 import { useRef, useState } from "react";
 import { useDropdownPosition } from "./use-dropdown-position";
 import { SubcategoryMenu } from "./subcategory-menu";
-
+import { CustomCategory } from "../types";
+import Link from "next/link";
 interface Props {
-    category: Category;
+    category: CustomCategory;
     isActive?: boolean;
     isNavigationHovered?: boolean;
 };
@@ -32,6 +33,8 @@ export const CategoryDropdown = ({
 
     const dropdownPosition = getDropdownPosition();
 
+
+
     return (
         <div
           className="relative"
@@ -44,10 +47,16 @@ export const CategoryDropdown = ({
                   variant="elevated"
                   className={cn(
                     "h11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
-                    isActive && !isNavigationHovered && "bg-white border-primary"
+                    isActive && !isNavigationHovered && "bg-white border-primary",
+                    isOpen && "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:-translate-x-1 hover:-translate-y-1"
+
                     )}
                 >
-                    {category.name}
+                    <Link
+                      href={`/${category.slug === "all" ? "" : category.slug}`}
+                    >
+                        {category.name}
+                    </Link>
                 </Button>
                 {category.subcategories && category.subcategories.length > 0 && (
                   <div className={cn(
