8b5c61a36aafa904e71d637bf42b3527658444f5
mcakunz <matheus.kunz05@gmail.com>
2025-12-29 16:59:29 -0300

    fix: enable superjson serialization

--- multi-tenent-e-commerce/src/trpc/init.ts
+++ multi-tenent-e-commerce/src/trpc/init.ts
@@ -2,12 +2,13 @@ import {initTRPC } from '@trpc/server'
 import { getPayload } from 'payload';
 import config from "@payload-config"
 import {cache} from 'react'
+import superjson from 'superjson';
 export const createTRPCContext = cache(async () => {
     return { userId: 'user_123'};
 });
 
 const t = initTRPC.create({
-
+    transformer: superjson,
 });
 
 export const createTRPCRouter = t.router;
