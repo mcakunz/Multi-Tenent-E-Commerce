6876c39fc600af44d833fb87e4ec29f432f45d45
mcakunz <matheus.kunz05@gmail.com>
2026-01-09 17:04:38 -0300

    fix: fix dropdown position getting lost & incorrect prefetching for infinite queries

--- multi-tenent-e-commerce/src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ multi-tenent-e-commerce/src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_LIMIT } from "@/constants";
 import { loadProductFilters } from "@/modules/products/search-params";
 import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
 import { ProductListView } from "@/modules/products/ui/views/product-list-view";
@@ -18,9 +19,10 @@ const Page = async ({ params, searchParams }: Props) => {
     const filters = await loadProductFilters(searchParams); 
     
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
-        category: subcategory,
+    void queryClient.prefetchInfiniteQuery(trpc.products.getMany.infiniteQueryOptions({
         ...filters,
+        category: subcategory,
+        limit: DEFAULT_LIMIT,
     }));
 
     return (
