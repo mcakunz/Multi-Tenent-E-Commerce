84fd4d494da3501e8e849bc7ead8b284500444fd
mcakunz <matheus.kunz05@gmail.com>
2026-01-13 18:47:04 -0300

    feat:  Product Page - Add "getOne" procedure  - Add "product-view.tsx" component    - /tenants/[slug]/products/[productId] - Modify "product-card.tsx" link href

--- multi-tenent-e-commerce/src/modules/products/server/procedures.ts
+++ multi-tenent-e-commerce/src/modules/products/server/procedures.ts
@@ -1,10 +1,30 @@
-import { Category, Media, Tenant } from "@/payload-types";
+import { Category, Media, Product, Tenant } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { getPayload, Sort, Where } from 'payload'
 import {z} from "zod";
 import { sortValues } from "../search-params";
 import { DEFAULT_LIMIT } from "@/constants";
 export const productsRouter = createTRPCRouter({
+    getOne: baseProcedure
+      .input(
+        z.object({
+          id: z.string(),
+        })
+      )
+      .query(async ({ ctx, input }) => {
+        const product = await ctx.db.findByID({
+          collection: "products",
+          id: input.id,
+          depth: 2, // Load the "product.image", "product.tenant" and "product.tenant.image"
+        });
+
+        return {
+          ...product,
+          image: product.image as Media | null,
+          tenant: product.tenant as Tenant & { image: Media | null },
+      };
+      }),
+
     getMany: baseProcedure.input(
       z.object({
         cursor: z.number().default(1),
