dac5849db765a6919119a12cfc72f5219ccf14ea
mcakunz <matheus.kunz05@gmail.com>
2026-01-30 14:41:55 -0300

    fix: Subdomain Rewrites - Update "generateTenantURL" logic - Fix cookie domain for subdomain access - Adjust Stripe URLs for subdomain routing

--- multi-tenent-e-commerce/src/lib/utils.ts
+++ multi-tenent-e-commerce/src/lib/utils.ts
@@ -6,9 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantURL(tenantSlug: string) {
-  if (process.env.NODE_ENV === "development"){
-    return `/tenants/${tenantSlug}`;
-  }
+   if (process.env.NODE_ENV === "development"){
+     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
+   }
 
   const protocol = "https";
   const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
